7efea4192de67aee501999df2869df3ea8e76f1b
Janko Jovičić <jankojovicic351@gmail.com>
2024-04-04 10:57:39 +0200

    Localized title in the static class by using context of the activity and the helper method.

--- WMS/App/DialogHelper.cs
+++ WMS/App/DialogHelper.cs
@@ -3,14 +3,24 @@ using Microsoft.AppCenter.Crashes;
 
 public static class DialogHelper
 {
+    private static int GetResourceIdString(Context context, string resourceName)
+    {
+        int id = context.Resources.GetIdentifier(resourceName, "string", context.PackageName);
+        // This method gets the corresponding id only for strings.
+        return id;
+    }
+
     public static void ShowDialogError(Activity activity, Context context, string error)
     {
         try
         {
             activity.RunOnUiThread(() =>
             {
+
+                string localizedTitle = context.GetString(GetResourceIdString(context, "s265"));
+
                 AlertDialog.Builder alert = new AlertDialog.Builder(context);
-                alert.SetTitle($"Error");
+                alert.SetTitle(localizedTitle);
                 alert.SetMessage(error);
                 alert.SetPositiveButton("Ok", (senderAlert, args) =>
                 {
@@ -32,8 +42,10 @@ public static class DialogHelper
         {
             activity.RunOnUiThread(() =>
             {
+                string localizedTitle = context.GetString(GetResourceIdString(context, "s268"));
+
                 AlertDialog.Builder alert = new AlertDialog.Builder(context);
-                alert.SetTitle("Information");
+                alert.SetTitle(localizedTitle);
                 alert.SetMessage(error);
                 alert.SetPositiveButton("Ok", (senderAlert, args) =>
                 {
